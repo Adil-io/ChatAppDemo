2919f83b52d4e18a6fe836e7db09d4bf23162a09
Adil-io <adilyusuf786@gmail.com>
2020-03-02 13:43:15 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 8080);
 
 var io = require('socket.io').listen(server);
 
